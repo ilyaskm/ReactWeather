8d3ea06571f4aec77d8fad6632795e6d7e39f480
Ilyas Karukappadath <ilyaskarukappadath@Ilyass-Air.fios-router.home>
2017-09-08 23:01:44 -0400

    added welcome to examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,6 +4,7 @@ var Examples = React.createClass({
     render : function() {
         return (
             <h2>Examples Component</h2>
+            <h2>Welcome to Examples</h2>
         )
     }
 });
