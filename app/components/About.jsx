4d5d04eba0fb6f875b772e19ca58134a92bc3e32
Ilyas Karukappadath <ilyasmohamed@hotmail.com>
2017-09-09 22:38:11 -0400

    style for About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,12 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var About = (props) => <h2>About Component New</h2>;
+var About = (props) => (
+    <div>
+        <h1 className='text-center'>About</h1>
+        <p>This is my first ReactJS application. I am learning ReactJS from Andrew through Udemy</p>
+        <a href='https://github.com/ilyaskm/ReactWeather' target='_new'>GitHub Locaiton</a>
+    </div>
+);
 
 module.exports = About;
