62aba2721e9c4c2238f681e66d074c8e978ccb48
Ilyas Karukappadath <ilyaskarukappadath@Ilyass-Air.fios-router.home>
2017-09-09 20:29:57 -0400

    updated middleware for handling localhost

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
     render : function() {
         return (
-            <h2>Examples Component</h2>
-            <h2>Welcome to Examples</h2>
+            <div>
+                <h2>Examples Component</h2>
+                <p>Welcome to Examples</p>
+            </div>
         )
     }
 });
